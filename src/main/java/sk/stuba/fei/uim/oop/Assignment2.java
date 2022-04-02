82d78e58674e58569e3db7997bda9ff1049499a6
SavkoMariiaAnastasiia <99816474+SavkoMariiaAnastasiia@users.noreply.github.com>
2022-04-02 17:18:54 +0300

    pridane triedy

--- src/main/java/sk/stuba/fei/uim/oop/Assignment2.java
+++ src/main/java/sk/stuba/fei/uim/oop/Assignment2.java
@@ -1,7 +1,8 @@
 package sk.stuba.fei.uim.oop;
+import sk.stuba.fei.uim.oop.hra.Hra;
 
 public class Assignment2 {
     public static void main(String[] args) {
-        
+        Hra hra = new Hra();
     }
 }
