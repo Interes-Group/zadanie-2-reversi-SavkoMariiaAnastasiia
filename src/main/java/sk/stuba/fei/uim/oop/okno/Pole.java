ae5c9683d2c52405af269e5fb5b670fd9a7e0f4b
SavkoMariiaAnastasiia <99816474+SavkoMariiaAnastasiia@users.noreply.github.com>
2022-04-07 03:32:55 +0300

    numerovane pola a pridana trieda kamne

--- src/main/java/sk/stuba/fei/uim/oop/okno/Pole.java
+++ src/main/java/sk/stuba/fei/uim/oop/okno/Pole.java
@@ -2,12 +2,14 @@ package sk.stuba.fei.uim.oop.okno;
 import sk.stuba.fei.uim.oop.hra.Hra;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import javax.swing.*;
 
-public class Pole extends Hra {
+public class Pole extends Hra implements ActionListener, MouseListener {
     public void setRozmer(int rozmer) {
         this.rozmer = rozmer;
     }
@@ -15,20 +17,25 @@ public class Pole extends Hra {
     public int rozmer = 6;
     public int stlbec = 250;
     public int riadok = 50;
+    private int x;
+    private int y;
     public Pole(){
     }
     public void GenPole(){
         int currnt=stlbec;
         JPanel pole_prehru = new JPanel();
         pole_prehru.setLayout(new GridLayout(rozmer,rozmer));
-        for (int i=0;i<rozmer;i++){
+        for ( x=0;x<rozmer;x++){
             currnt=currnt+37;
             riadok = 50;
-            for (int j=0;j<rozmer;j++){
+            for (y=0;y<rozmer;y++){
                 JPanel poli=new JPanel();
+                JLabel tpoli =new JLabel(x+"."+y);
                 poli.setBounds(currnt,riadok,35,35);
                 poli.setBackground(new Color(236, 117, 46));
                 riadok=riadok+37;
+                poli.add(tpoli);
+                tpoli.addMouseListener(this);
                 pole_prehru.add(poli);
                 frame.add(poli);
             }
@@ -37,5 +44,35 @@ public class Pole extends Hra {
 //        for ( int i=0;i<pole.toArray().length;i++){
 //            System.out.println(pole);
 //        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(e.getActionCommand());
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println(e.getLocationOnScreen()+""+x+"."+y);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
